7f5ce362887c54b3b293c88acf786c2bb97bfbe6
TranTrungPhong <achinpo1999@gmail.com>
2024-10-31 21:44:07 +0700

    addlan2

--- src/main/java/com/example/wtfProject/HelloServlet.java
+++ src/main/java/com/example/wtfProject/HelloServlet.java
@@ -15,7 +15,7 @@ public class HelloServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-        System.out.println("vl luon");
+        System.out.println("Minh Phee");
 
         // Hello
         PrintWriter out = response.getWriter();
